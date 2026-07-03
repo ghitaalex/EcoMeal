08814a2845b47e2f0488fb059650c9315b6b96fd
Ghita Alex <ghitaalex66@gmail.com>
2026-07-03 10:56:56 +0300

    Added Entities and Infrastructure'

---
+++ EcoMeal.Api/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace EcoMeal.Api.Entities
+{
+    public class User 
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Contact { get; set; }
+    }
+}
---
+++ EcoMeal.Api/Infrastructure/EcoMealDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EcoMeal.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoMeal.Api.Infrastructure
+{
+    public class EcoMealDbContext : DbContext
+    {
+        public EcoMealDbContext(DbContextOptions<EcoMealDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
