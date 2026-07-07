74c9542eadc638a9e4d9f7bc5b491ad5d48ca6f8
Ghita Alex <ghitaalex66@gmail.com>
2026-07-07 17:43:12 +0300

    Added MudBlazor and worked on design

--- EcoMeal.Client/Program.cs
+++ EcoMeal.Client/Program.cs
@@ -1,11 +1,12 @@
 using EcoMeal.Client.Components;
 using EcoMeal.Client.Services;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddMudServices();
 builder.Services.AddHttpClient("EcoMealApi", client =>
 {
     client.BaseAddress = new Uri("https://localhost:7088/");
