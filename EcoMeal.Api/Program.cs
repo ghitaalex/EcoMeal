bb4a84a5a8b78f11c40a37ddd9e777acdbc2c9f1
Ghita Alex <ghitaalex66@gmail.com>
2026-07-15 10:22:59 +0300

    Enable Swagger in all environments

--- EcoMeal.Api/Program.cs
+++ EcoMeal.Api/Program.cs
@@ -52,13 +52,10 @@ builder.Services.AddScoped<EmailService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwaggerUI(options => {
-        options.SwaggerEndpoint("/openapi/v1.json", "EcoMeal API");
-    });
-}
+app.MapOpenApi();
+app.UseSwaggerUI(options => {
+    options.SwaggerEndpoint("/openapi/v1.json", "EcoMeal API");
+});
 
 app.UseHttpsRedirection();
 
